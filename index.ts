870f9bcd6d43fab1eab82f4d5037653cae11844a
pedropeglow <pedropeglowm@gmail.com>
2022-12-11 11:31:56 -0300

    :sparkles: Adicionando cors para fazer chamada

--- index.ts
+++ index.ts
@@ -4,10 +4,12 @@ Connection;
 import rotasCliente from "./routes/clientesRoutes";
 import rotasLivro from "./routes/livrosRoutes";
 import rotasAutor from "./routes/autoresRoutes";
+var cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 const PORT = 3000;
 
 const server = app.listen(3000, () => {
